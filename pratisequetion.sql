2fd81133f9a6ed91c5154f583dc365260446ec12
harshkachhaway <harshkachhaway8001@gmail.com>
2023-08-23 22:28:54 +0530

    solved quetion no 6,7

--- pratisequetion.sql
+++ pratisequetion.sql
@@ -73,3 +73,47 @@ SELECT substring(year,1,2)+1 AS centuries FROM container3;
  CASE WHEN MOD(value,2) =0 THEN "EVEN"
       WHEN MOD(value,2) !=0 THEN "ODD"
  END AS EVEN_ODD FROM container4;
+
+                              -- PRATICE QUETION NO.5
+							
+CREATE TABLE container5(
+id INT PRIMARY KEY,
+name VARCHAR(50),
+age INT );
+
+INSERT INTO container5 VALUES
+(1,"BOB",21),
+(2,"SAM",19),
+(3,"JILL",18),
+(4,"JIM",21),
+(5,"SALLY",19),
+(6,"JESS",20),
+(7,"WILL",21);
+
+DROP TABLE container5 ;
+
+SELECT age,COUNT(age) as count FROM container5 
+GROUP BY age
+ORDER BY age ASC;
+
+      -- QUETION NO.6
+      
+CREATE TABLE container6(
+id INT PRIMARY KEY,
+name VARCHAR(50));
+
+INSERT INTO container6 VALUES
+(1,"BOB"),
+(2,"SAM"),
+(3,"JILL"),
+(4,"JIM"),
+(5,"SALLY"),
+(6,"JESS"),
+(7,"WILL");
+
+SELECT CONCAT("Hi, ",name,"! how are you today") AS Greating
+FROM container6;
+
+
+                             -- PRATICE QUETION NO.7
+
